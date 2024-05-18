eb675772a685ec287dc54e401c5624b1976f9d15
heedaelee <adguy@5works.co.kr>
2024-05-18 14:53:20 +0900

    featrue : first commit
    처음 커밋 몽땅 올리기

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # vue_typescript
+정토회 스터디 모임서 강연하는 vue3에 타입스크립트 입히기
+타입스크립트 강의입니당. 
+그중에 git api 받아서 타입 처리하는 코드 example 입니당!!
+
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <h1>깃헙 유저 서치 엔진</h1>
+    <input
+      v-model="username"
+      placeholder="깃헙 아이디를 입력하세요"
+      @keyup.enter="searchUser"
+    />
+    <div v-if="user">
+      <h2>{{ user.name }}</h2>
+      <p>{{ user.bio }}</p>
+      <img :src="user.avatar_url" alt="User Avatar" />
+      <p>
+        <a :href="user.html_url" target="_blank">프로필 보기</a>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from "axios";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+/**
+ * TODO: 타입을 걸어봅시다!
+ */
+
+interface IgitHubUser {
+  name: string;
+  bio: string;
+  avatar_url: string;
+  html_url: string;
+}
+
+const username = ref<string>("");
+const user = ref<IgitHubUser | null>(null);
+
+const searchUser = async () => {
+  if (username.value.trim() !== "") {
+    try {
+      const response = await axios.get<IgitHubUser>(
+        `https://api.github.com/users/${username.value}`
+      );
+      console.log(`response`, response);
+      user.value = response.data;
+    } catch (error) {
+      console.error("에러 발생", error);
+      user.value = null;
+    }
   }
-});
+};
 </script>
 
 <style>
